1c0863c96357b84218bbe012084224ee760c34c0
Kyonea <cooperation.justa.stobiecka@gmail.com>
2022-04-11 22:40:19 +0200

    changed the layout to the temporary, changed the links and routing to single page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from './about/about.component';
 import {ContactComponent} from './contact/contact.component';
 import {MyProjectsComponent} from './my-projects/my-projects.component';
+import {MainContentComponent} from './main-content/main-content.component';
 
 const routes: Routes = [
 // dodany routing podstawowy
-  {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent},
+  {path: 'about', component: MainContentComponent},
   {path: 'my-projects', component: MyProjectsComponent},
   {path: '', redirectTo:'about' , pathMatch: 'full'}
 
