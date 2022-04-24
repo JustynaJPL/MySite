b0a76eeab7a947496f456dbe5672fc09cd96876d
Kyonea <cooperation.justa.stobiecka@gmail.com>
2022-04-24 22:04:33 +0200

    fixed issue with json

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import projects from "./project.info.json";
+//json imported from the file to load the content
 
 
 @Component({
@@ -10,24 +12,17 @@ import { Component, OnInit } from '@angular/core';
 
 export class MyProjectsComponent implements OnInit {
 
-  projects = 
-    [
-    {
-      id: 1, name: 'Angular Tutorial', category:'Angular', 
-      description: ' Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ', 
-      image: './assets/angular_logo.png',
-      link: 'https://justynajpl.github.io/AngularTutStore/', 
-      progress: '99%'
-    },
-    {
-      id: 2, name: 'Angular Tutorial Heroes App', category:'Angular', 
-      description: ' Tour the Heroes app Angular tutorial from the https://angular.io/tutorial/toh-pt1  ', 
-      image: './assets/angular_logo.png',
-      link: 'https://github.com/JustynaJPL/tourtheheroesapp', 
-      progress: '30%'
-    },
-
-    ];
+//this is the def of project component it can be expanded if needed
+  projects:{
+        id:number, 
+        name:string,
+        category:string,
+        description:string,
+        image:string,
+        link:string,
+        progress:number
+  }[] = projects;
+  
 
   constructor() { 
    
